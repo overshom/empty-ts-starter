93143364c0f7c2eed9023bb1abb88ed377c2982c
Oleksii Shnyra <shnura94@gmail.com>
2023-12-13 23:18:02 +0100

    feat: show base stats

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@ const port = 18325;
 
 app.use(cors());
 
+const started = new Date();
+const instanceId = `id-${Math.floor(Math.random() * 1e13)}`;
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const elapsed = Date.now() - started.getTime();
+    res.send({ elapsed, started: started.toJSON(), instanceId });
 });
 
 app.listen(port, () => {
