e20a9b943d31257ed604c10b43404b2411548369
Oleksii Shnyra <shnura94@gmail.com>
2023-12-17 12:37:50 +0100

    prettier: tab width 2

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ const started = new Date();
 const instanceId = `id-${Math.floor(Math.random() * 1e13)}`;
 
 app.get('/', (req, res) => {
-    const elapsed = Date.now() - started.getTime();
-    res.send({ elapsed, started: started.toJSON(), instanceId });
+  const elapsed = Date.now() - started.getTime();
+  res.send({ elapsed, started: started.toJSON(), instanceId });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
+  console.log(`Example app listening on http://localhost:${port}`);
 });
